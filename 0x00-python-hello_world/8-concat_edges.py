fbb2fa3253cebbfcefadc69e587d6e3b351e3573
BlackEryque <ericksonamuyunzu@outlook.com>
2023-03-23 18:19:01 +0300

    Task8 now

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an intepreted, interactive, object-oriented programming\
+        language that combines remarakble power with very clear syntax"
+str = str[38:65] + ' ' + str[113:117] + ' ' + str[0:6]
+print(str)
