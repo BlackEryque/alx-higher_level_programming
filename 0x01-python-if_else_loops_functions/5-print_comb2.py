a52d27466d9b7ec9afc7d6d55f98cf1ca0ff93d9
BlackEryque <ericksonamuyunzu@outlook.com>
2023-03-28 22:06:08 +0300

    Task5 ammended

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if (i == 99):
-        print(f"{i:2}")
+        print("{}".format(i))
     else:
-        print(f"{i:0^2},", end=" ")
+        print("{:02},".format(i), end=" ")
