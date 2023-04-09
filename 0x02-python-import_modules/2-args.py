91fe94910ca11b14efe0892d394be4976f0c8e58
BlackEryque <ericksonamuyunzu@outlook.com>
2023-04-09 18:01:34 +0300

    args task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     if (num_elmts - 1) == 0:
         print("{} arguements.".format(num_elmts - 1))
     else:
-        print("{} arguements:".format(num_elmts))
+        print("{} arguements:".format(num_elmts - 1))
         for i in range(1, num_elmts):
             print("{}: {}".format(i, sys.argv[i]))
