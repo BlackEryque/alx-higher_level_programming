ebc0fb1a15d549f0f66e619f48cf5f46585fc9a5
BlackEryque <ericksonamuyunzu@outlook.com>
2023-04-10 21:07:19 +0300

    argu

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     elif (num_elmts - 1) == 1:
         print("{} argument:".format(num_elmts - 1))
         for i in range(1, num_elmts):
-            print("{}: {}".format(i, sys.argv[i]))  
+            print("{}: {}".format(i, sys.argv[i]))
     else:
         print("{} arguments:".format(num_elmts - 1))
         for i in range(1, num_elmts):
