447c15348a48f82324f1d06d6274d0080c3ff44e
BlackEryque <ericksonamuyunzu@outlook.com>
2023-04-10 21:02:10 +0300

    argu

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
 
     if (num_elmts - 1) == 0:
         print("{} arguements.".format(num_elmts - 1))
+    elif (num_elmts - 1) == 1:
+        print("{} arguement:".format(num_elmts - 1))
+        for i in range(1, num_elmts):
+            print("{}: {}".format(i, sys.argv[i]))  
     else:
-        print("{} arguements:".format(num_elmts - 1))
+        print("{} arguements:".format(num_elmts))
         for i in range(1, num_elmts):
             print("{}: {}".format(i, sys.argv[i]))
